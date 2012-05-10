89e5b0a70a947ac29325f0347de8683a821aa700
dozer <mail@dozer.cc>
2012-05-10 13:25:58 +0800

    import

---
+++ src/DAL/DataBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using MODEL;
+
+namespace DAL
+{
+    public class DataBase : DbContext
+    {
+        public DataBase() : base("DefaultConnection") { }
+        public DbSet<User> UserSet { get; set; }
+    }
+}
---
+++ src/MODEL/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MODEL
+{
+    [Serializable]
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string NickName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime FirstLoginDate { get; set; }
+        public DateTime LastLoginDate { get; set; }
+        public int LoginTimes { get; set; }
+    }
+}
